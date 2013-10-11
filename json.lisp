2d3c4142c412a8cdd9c398362d01fc0112e1072a
Jeffrey Massung <massung@gmail.com>
2013-10-11 11:31:37 -0500

    Improved pretty printing functionality.

--- json.lisp
+++ json.lisp
@@ -185,12 +185,12 @@
 (defmethod json-encode-into ((value (eql t)) &optional (*standard-output* *standard-output*))
   "Encode the true value."
   (declare (ignore value))
-  (write-string "true"))
+  (format t "~<true~>"))
 
 (defmethod json-encode-into ((value (eql nil)) &optional (*standard-output* *standard-output*))
   "Encode the null constant."
   (declare (ignore value))
-  (write-string "null"))
+  (format t "~<null~>"))
 
 (defmethod json-encode-into ((value symbol) &optional (*standard-output* *standard-output*))
   "Encode a symbol to a stream."
@@ -198,11 +198,11 @@
 
 (defmethod json-encode-into ((value number) &optional (*standard-output* *standard-output*))
   "Encode a number to a stream."
-  (write value))
+  (format t "~<~a~>" value))
 
 (defmethod json-encode-into ((value ratio) &optional (*standard-output* *standard-output*))
   "Encode a ratio to a stream."
-  (write (float value)))
+  (format t "~<~a~>" (float value)))
 
 (defmethod json-encode-into ((value string) &optional (*standard-output* *standard-output*))
   "Encode a string as a stream."
@@ -219,7 +219,7 @@
              (format nil "\\u~16,4,'0r" (char-code c)))
             (t
              (string c)))))
-    (format t "\"~{~a~}\"" (map 'list #'encode-char value))))
+    (format t "~<\"~{~a~}\"~>" (map 'list #'encode-char value))))
 
 (defmethod json-encode-into ((value vector) &optional (*standard-output* *standard-output*))
   "Encode an array to a stream."
@@ -227,15 +227,15 @@
         (*print-level* nil)
         (*print-length* nil)
         (*print-lines* nil)
-        (*print-right-margin* 14))
+        (*print-right-margin* 72))
     (pprint-logical-block (*standard-output* nil :prefix "[" :suffix "]")
       (when (plusp (length value))
         (json-encode-into (aref value 0)))
       (loop :for i :from 1 :below (length value)
             :do (progn
                   (write-char #\,)
-                  (pprint-newline :linear)
-                  (pprint-indent :current 0)
+                  (pprint-newline :fill)
+                  (pprint-indent :block 0)
                   (json-encode-into (aref value i)))))))
 
 (defmethod json-encode-into ((value standard-object) &optional (*standard-output* *standard-output*))
@@ -244,7 +244,7 @@
         (*print-level* nil)
         (*print-length* nil)
         (*print-lines* nil)
-        (*print-right-margin* 14))
+        (*print-right-margin* 72))
     (pprint-logical-block (*standard-output* (class-slots (class-of value)) :prefix "{" :suffix "}")
       (pprint-exit-if-list-exhausted)
       (loop
@@ -254,5 +254,5 @@
          (json-encode-into (slot-value value name))
          (pprint-exit-if-list-exhausted)
          (write-char #\,)
-         (pprint-newline :linear)
+         (pprint-newline :mandatory)
          (pprint-indent :current 0))))))
